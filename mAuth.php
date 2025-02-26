d6230cbf0525214e37af5539752b719bad603f3f
Bagus Ananta Hidayatullah <68620177+BagusAnanta@users.noreply.github.com>
2025-02-26 21:53:42 +0700

    Update mAuth.php
    update query command

--- mAuth.php
+++ mAuth.php
@@ -1,16 +1,17 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+// maybe on this query execute, we get password by username only
+// so user not get more user data
+// so a query command is : SELECT password FROM user WHERE 'username' = username
+
 class mAuth extends CI_Model
 {
     public function Auth($username, $password)
     {
-        $this->db->select('*');
+        $this->db->select($password);
         $this->db->from('user');
-        $this->db->where(array(
-            'username' => $username,
-            'password' => $password
-        ));
+        $this->db->where('username', $username);
         return $this->db->get()->row();
     }
 }
